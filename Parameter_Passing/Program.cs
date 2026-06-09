c1fad3c7d6bf1a664d5af4d0802d5de61cea730e
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-06-09 12:57:33 +0530

    new line pushed

--- Parameter_Passing/Program.cs
+++ Parameter_Passing/Program.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Authentication.ExtendedProtection.Configuration;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Authentication.ExtendedProtection.Configuration;
+//using System.Security.Principal;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Parameter_Passing
-{
-    internal class Program
-    {
-        class student
-        {
-            int sid;
-            string sname;
-            int sage;
-            int total;
-            int avg;
+//namespace Parameter_Passing
+//{
+//    internal class Program
+//    {
+//        class student
+//        {
+//            int sid;
+//            string sname;
+//            int sage;
+//            int total;
+//            int avg;
 
            
-            public void studentdisplay( out int sid, string sname, int sage)
-            {
-                sid = 101;
-                Console.WriteLine("student id is " + sid);
-                Console.WriteLine("student name is " + sname);
-                Console.WriteLine("student age is " + sage);
+//            public void studentdisplay( out int sid, string sname, int sage)
+//            {
+//                sid = 101;
+//                Console.WriteLine("student id is " + sid);
+//                Console.WriteLine("student name is " + sname);
+//                Console.WriteLine("student age is " + sage);
 
-            }
-            public void marksdisplay( ref int m1,int m2,int m3)
-            {
+//            }
+//            public void marksdisplay( ref int m1,int m2,int m3)
+//            {
                 
-                total = m1 + m2 + m3;
-                avg = (m1 + m2 + m3) / 3;
-                if(avg >= 90)
-                {
-                    Console.WriteLine("Grade A");
-                }
-                else if (avg >= 80)
-                {
-                    Console.WriteLine("Grade B");
-                }
-                else if (avg >= 35)
-                {
-                    Console.WriteLine("Grade C");
-                }
-                else  
-                {
-                    Console.WriteLine("Grade F");
-                }
-            }
+//                total = m1 + m2 + m3;
+//                avg = (m1 + m2 + m3) / 3;
+//                if(avg >= 90)
+//                {
+//                    Console.WriteLine("Grade A");
+//                }
+//                else if (avg >= 80)
+//                {
+//                    Console.WriteLine("Grade B");
+//                }
+//                else if (avg >= 35)
+//                {
+//                    Console.WriteLine("Grade C");
+//                }
+//                else  
+//                {
+//                    Console.WriteLine("Grade F");
+//                }
+//            }
 
-        }
+//        }
         
-        static void Main(string[] args)
-        {
-            student obj = new student();
-            int m1 = 85;
-            obj.marksdisplay(ref m1,90,90);
-            int sid=105;
-            obj.studentdisplay( out sid,"rama",17);
-        }
-    }
-}
+//        static void Main(string[] args)
+//        {
+//            student obj = new student();
+//            int m1 = 85;
+//            obj.marksdisplay(ref m1,90,90);
+//            int sid=105;
+//            obj.studentdisplay( out sid,"rama",17);
+//        }
+//    }
+//}
---
+++ Parameter_Passing/passingparameters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Passing
+{
+    internal class passingparameters
+    { 
+        class student
+        {
+            public void studentdisplay(int sid, string sname)
+            {
+                Console.WriteLine("student id is " + sid);
+                Console.WriteLine("student name is " + sname);
+            }
+            public void updateage(ref int age)
+            {
+                age += 1;
+                Console.WriteLine("student updatedage is " + age);
+            }
+            public void studentmarks(int m1, int m2, int m3, out int total)
+            {
+                total = m1 + m2 + m3;
+                Console.WriteLine("total marks is " + total);
+            }
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                student obj = new student();
+                obj.studentdisplay(3, "raju");
+                int age = 22;
+                obj.updateage(ref age);
+                int x;
+                obj.studentmarks(89, 99, 79, out x);
+            }
+        }
+    }
+}
+
