614704516a5a1bfde8a2bc6da71f26b309bbc6f5
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-06-09 14:51:31 +0530

    new line pushed

---
+++ Parameter_Passing/staticcls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Passing
+{
+    internal class staticcls
+    {
+        static class myclass
+        {
+            static int a;
+            static myclass()
+            {
+                a = 10;
+            }
+            internal static void display()
+            {
+                Console.WriteLine(" a value is " + a);
+            }
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                myclass.display();
+            }
+        }
+    }
+}
