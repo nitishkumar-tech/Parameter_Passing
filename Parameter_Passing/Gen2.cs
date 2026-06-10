0f9d5e0433682b5efa38d769e5d29ec8027d0f06
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-06-10 15:43:57 +0530

    send new line

---
+++ Parameter_Passing/Gen2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Passing
+{
+    class student
+    {
+        public void studentdisplay<T, K>(T sid, K sname)
+        {
+            Console.WriteLine("student id is " + sid);
+            Console.WriteLine("student name is " + sname);
+        }
+        public void updateage<T>(ref T age)
+        {
+            age = (dynamic)age + 1;
+            Console.WriteLine("student updatedage is " + age);
+        }
+        public void studentmarks<T>(T m1, T m2, T m3, out T total)
+        {
+            total = (dynamic)m1 + (dynamic)m2 + (dynamic)m3;
+            Console.WriteLine("total marks is " + total);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            student obj = new student();
+            obj.studentdisplay(3, "raju");
+            int age = 22;
+            obj.updateage(ref age);
+            int x;
+            obj.studentmarks(89, 99, 79, out x);
+        }
+    }
+}
+
---
+++ Parameter_Passing/gen1.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Parameter_Passing
+//{
+//    class myclass
+//    {
+//        internal void display<T>(T u)
+//        {
+//            Console.WriteLine(u);
+//        }
+//        internal void add<t>(t a, t b)
+//        {
+//            Console.WriteLine("Value of a is " + a);
+//            Console.WriteLine("Value of b is " + b);
+//        }
+//    }
+//    internal class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass obj = new myclass();
+//            obj.display(30);
+//            obj.display<string>("Nvidia");
+//            obj.display<bool>(true);
+//            obj.display<double>(3.90);
+//            obj.add<int>(40, 60);
+//            obj.add<double>(4.98, 6.89);
+//            obj.add("hello", "user");
+//            obj.add<bool>(true, false);
+//        }
+//    }
+//}
+
