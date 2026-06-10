fd83ab7ddb1d2b9d8209fcad10c9a417894af75e
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-06-10 12:35:19 +0530

    new line pushed

--- Parameter_Passing/PartialPatienr-2.cs
+++ Parameter_Passing/PartialPatienr-2.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Parameter_Passing
-{
-    public partial class Patient
-    {
-        public void GenerateBill()
-        {
-            Console.WriteLine("Bill Generated");
-        }
-    }
-}
+//namespace Parameter_Passing
+//{
+//    public partial class Patient
+//    {
+//        public void GenerateBill()
+//        {
+//            Console.WriteLine("Bill Generated");
+//        }
+//    }
+//}
--- Parameter_Passing/PartialPatient-1.cs
+++ Parameter_Passing/PartialPatient-1.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Parameter_Passing
-{
-    public partial class Patient
-    {
-        public void AdmitPatient()
-        {
-            Console.WriteLine("Patient Admitted");
-        }
-    }
-}
+//namespace Parameter_Passing
+//{
+//    public partial class Patient
+//    {
+//        public void AdmitPatient()
+//        {
+//            Console.WriteLine("Patient Admitted");
+//        }
+//    }
+//}
